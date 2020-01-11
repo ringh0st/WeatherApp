3eedea81695a80005e56e71725ba9621860a3763
Rinat Nadav <ringhost@gmail.com>
2020-01-11 22:39:12 +0200

    finished

--- script.js
+++ script.js
@@ -1,57 +1,70 @@
 const data = `www.google.com/q=london.com`
 const weather = {};
-weather.request = new XMLHttpRequest()
-weather.request.open('GET', 'api.openweathermap.org/data/2.5/forecast?q=${city name},{country code}', true)
+// weather.request = new XMLHttpRequest()
+// weather.request.open('GET', 'api.openweathermap.org/data/2.5/forecast?q=${city name},{country code}', true)
 weather.start=()=>{
     searchWeather()
+    shuffleImages()
 }
 
 const searchWeather = ()=>{
-    let city = document.querySelector('.cityName');
-    let currentTime = document.querySelector('.currentTimeName');
-    let weeklyWeather = document.querySelector('.weeklyWeatherName');
     let inputValue = document.querySelector('#searchBar');
-
     const btn = document.querySelector('.btn');
-    const introPage = document.querySelector(".hero");
-    let mainPage = document.querySelector(".mainPage");
+    const hero = document.querySelector(".hero");
+    const mainPage = document.querySelector('.mainPage')
+    mainPage.style.display = "none";
     btn.addEventListener("click", enterPage=()=>{
-        // introPage.style.display = "none"
-        // mainPage.style.display = "block"
-        // // fetch('https://api.openweathermap.org/data/2.5/forecast?q='+inputValue.value+'7cf2ce9913cb1e81c5f1070de180744a',{mode:'no-cors'})
-        // fetch('https://api.aerisapi.com/forecasts/55415?client_id=sF3acNgrpJAlMVvDmmlIM&client_secret=LJ4BlKxyxHaSFhr8tPS0BUmKxUrTpn2gIDUxFUGh')
-        // .then(response => response.json())
-        // .then(data => console.log(data))
-        // .catch(err => alert("wrong city name!"))
-        
-        // console.log(fetch)
-        // load the latest observation for Seattle, WA
-aeris.api().endpoint('observations').place('seattle,wa').get().then((result) => {
-    const data = result.data.ob;
-    document.getElementById('obs').innerHTML = `The current weather is ${data.weatherPrimary.toLowerCase()} and ${data.tempF} degrees.`;
-});
+        mainPage.style.display = "flex";
+        hero.style.display = "none";
+        fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=7cf2ce9913cb1e81c5f1070de180744a')
+            .then(response =>{
+                let data = response.json()
+            return data;
+        })
+            // .then(function(data){
+            //     weather.temperature.value = data.main.temp
+            // })
+            .then(blob =>{
+                let feelsLike = blob.main.feels_like -273.15;
+                let temp = blob.main.temp -273.15;
+                let name = blob.name;
+                let weatherDescription = (blob.weather[0].description);
+                let windSpeed = blob.wind.speed
+                let humidity = (blob.main.humidity)
 
-// load the latest radar image for Seattle, WA at 500x300 pixels
-aeris.map().layers('flat,radar,counties,admin').center('seattle,wa').zoom(9).size(500, 300).get().then((result) => {
-    // append result image to a DOM target
-    document.getElementById('map-target').appendChild(result.image);
-    
-    // output image valid time to a DOM target
-    document.getElementById('map-metadata').innerHTML = `Valid: ${result.metadata.validDate}`;
-});
-    })
+                document.querySelector('.humidity').innerHTML = "humidity :" + humidity +"%"
+                document.querySelector('.weatherDescription').innerHTML = "The sky with " + weatherDescription
+                document.querySelector('.cityName').innerHTML = name
+                document.querySelector('.temp').innerHTML = "The current tempature is " + Math.floor(temp) + "c"
+                document.querySelector(".feelsLike").innerHTML = "Probably feels like " + Math.floor(feelsLike) + "c";
+                document.querySelector('.windSpeed').innerHTML = "wind Speed " + windSpeed + "ms"
+        })
+            .catch(error =>{
+                document.querySelector('.error').innerHTML = "please enter a valid city name"
+        })
+})
 }
+
+const shuffleImages = () =>{
+    
+        var random= Math.floor(Math.random() * 6) + 0;
+        var bigSize = ["url('./images/backImg/1.jpeg')",
+                       "url('./images/backImg/2.jpeg')",
+                       "url('./images/backImg/3.jpeg')",
+                       "url('./images/backImg/4.jpeg')",
+                       "url('./images/backImg/5.jpeg')",
+                       "url('./images/backImg/6.jpeg')",
+                       "url('./images/backImg/7.jpeg')",
+                       "url('./images/backImg/8.jpeg')",
+                       "url('./images/backImg/9.jpeg')",
+                       "url('./images/backImg/10.jpeg')",
+                       "url('./images/backImg/11.jpeg')",
+                       "url('./images/backImg/12.jpeg')",
+                       "url('./images/backImg/13.jpeg')",
+                       "url('./images/backImg/14.jpeg')",
+                       "url('./images/backImg/15.jpeg')",
+                    ];
+        document.querySelector(".mainPage").style.backgroundImage=bigSize[random];
+      }
+
 weather.start()
-// fetch("https://weatherbit-v1-mashape.p.rapidapi.com/forecast/3hourly?units=M&lang=en&lat=-78&lon=37", {
-// 	"method": "GET",
-// 	"headers": {
-// 		"x-rapidapi-host": "weatherbit-v1-mashape.p.rapidapi.com",
-// 		"x-rapidapi-key": "1ca9a95402mshee05a170c9b108bp1d363djsn3328a6420e74"
-// 	}
-// })
-// .then(response => {
-// 	console.log(response);
-// })
-// .catch(err => {
-// 	console.log(err);
-// });
